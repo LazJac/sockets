b05e6e0b077ddbe1c796f7c393e62ac1194a74e8
LazJac <vwykjacoj@gmail.com>
2019-10-08 12:22:06 +0200

    Server function init

--- Server.java
+++ Server.java
@@ -5,13 +5,50 @@ import java.io.*;
 public class Server
 {
 	//Input stream defined
-	private Socket socket = null; //null by default before inputing it
+	private Socket socket = null; 
 	private ServerSocket server = null;
 	private DataInputStream in = null;
+	
+	public Server(int port)
+	{
+		try
+		{
+			server = new ServerSocket(port);
+			System.out.println("Server Started");
+			
+			System.out.println("Waiting for a client to connect ...");
+			
+			socket = server.accept();
+			System.out.println("Client connection accepted");
+			
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			String line = "";
+			
+			While(!line.equals("Done"))
+			{
+				try
+				{
+					line = in.readUTF();
+					system.out.println(line);
+				}
+				catch(IOException i)
+				{
+					System.out.println(i);
+				}
+			}
+			
+			socket.close();
+			in.close();
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+	}
 }
 
     public static void main(String args[])
     {
         Server server = new Server(9999);
     }
-}
+
